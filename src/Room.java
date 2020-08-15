1d906e307621b188390dd5c4434e2909359a7c2e
VincentPreikstas <43098530+VincentPreikstas@users.noreply.github.com>
2020-08-15 18:01:30 -0400

    8/15/2020
    Test

--- src/Room.java
+++ src/Room.java
@@ -5,19 +5,11 @@ public class Room {
     Room[] roomArray;
 
 
-
-
-    public Room(){
-
-    }
     public Room(String roomName, String description) {
         this.roomName = roomName;
         this.description = description;
         this.roomArray = new Room[4];
-        Adult[] adultArray = new Adult[5];
-        this.characterArray = adultArray;
-        for (int i = 0; i < adultArray.length; i++) {
-        }
+        this.characterArray = new Character[5];
     }
 
     public String toString() {
